8bf0f35963d8d1cc1cf792f9c197b2ea1e81a60e
yoon <yoons2owo@gmail.com>
2021-04-24 23:01:12 +0900

    백준 1차원 배열 최소 최대

--- src/main/java/com/yoons2owo/baekjoon/array/최소최대/Main.java
+++ src/main/java/com/yoons2owo/baekjoon/array/최소최대/Main.java
@@ -1,11 +1,11 @@
-package com.yoons2owo.baekjoon.array;
+package com.yoons2owo.baekjoon.array.최소최대;
 
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Arrays;
 
-public class 최소최대 {
+public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int input = Integer.parseInt(br.readLine());
