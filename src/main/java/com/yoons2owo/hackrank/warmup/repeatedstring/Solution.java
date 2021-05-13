824d12a389a2dcab2237c088ab86948c1587457b
yoon <yoons2owo@gmail.com>
2021-05-13 23:46:46 +0900

    해커랭크 warm up Repeated String

---
+++ src/main/java/com/yoons2owo/hackrank/warmup/repeatedstring/Solution.java
@@ -0,0 +1,59 @@
+package com.yoons2owo.hackrank.warmup.repeatedstring;
+
+import java.io.*;
+import java.math.*;
+import java.security.*;
+import java.text.*;
+import java.util.*;
+import java.util.concurrent.*;
+import java.util.function.*;
+import java.util.regex.*;
+import java.util.stream.*;
+import static java.util.stream.Collectors.joining;
+import static java.util.stream.Collectors.toList;
+
+class Result {
+
+    /*
+     * Complete the 'repeatedString' function below.
+     *
+     * The function is expected to return a LONG_INTEGER.
+     * The function accepts following parameters:
+     *  1. STRING s
+     *  2. LONG_INTEGER n
+     */
+
+    public static long repeatedString(String s, long n) {
+        // Write your code here
+        String[] ar = s.split("");
+        long aCnt = Arrays.stream(ar).filter(e -> "a".equals(e)).count() * (n / ar.length);
+        if ( n % ar.length != 0 ) {
+            for (int i = 0; i < n % ar.length; i++) {
+                if ("a".equals(ar[i])) {
+                    aCnt++;
+                }
+            }
+        }
+        return aCnt;
+    }
+
+}
+
+public class Solution {
+    public static void main(String[] args) throws IOException {
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bufferedWriter = new BufferedWriter(new FileWriter(System.getenv("OUTPUT_PATH")));
+
+        String s = bufferedReader.readLine();
+
+        long n = Long.parseLong(bufferedReader.readLine().trim());
+
+        long result = Result.repeatedString(s, n);
+
+        bufferedWriter.write(String.valueOf(result));
+        bufferedWriter.newLine();
+
+        bufferedReader.close();
+        bufferedWriter.close();
+    }
+}
