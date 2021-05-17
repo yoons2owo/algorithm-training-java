8013c5a37fe01a50fa56823cf6eb2b0a6b325653
yoon <yoons2owo@gmail.com>
2021-05-18 01:56:16 +0900

    프로그래머스 정렬 가장 큰 수

--- src/main/java/com/yoons2owo/programmers/sort/가장큰수/Solution.java
+++ src/main/java/com/yoons2owo/programmers/sort/가장큰수/Solution.java
@@ -6,7 +6,7 @@ import java.util.Comparator;
 public class Solution {
     public static void main(String[] args) {
         Solution sol = new Solution();
-        System.out.println(sol.solution(new int[]{6, 10, 2}));
+        System.out.println(sol.solution(new int[]{6, 0, 10, 2}));
     }
 
     public String solution(int[] numbers) {
@@ -15,12 +15,12 @@ public class Solution {
         for (int i = 0; i < numbers.length; i++) {
             arr[i] = String.valueOf(numbers[i]);
         }
-        Arrays.sort(arr, new Comparator<String>() {
-            @Override
-            public int compare(String o1, String o2) {
-                return (o1+o2).compareTo(o2+o1);
-            }
-        });
+        Arrays.sort(arr, (o1, o2) -> (o2 + o1).compareTo(o1 + o2));
+
+        if (arr[0].equals("0")) {
+            return "0";
+        }
+
         for (int i = 0; i < arr.length; i++) {
             answer += arr[i];
         }
